f8e9cb6436f3449b0ac4350d5f56b495894c54ce
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2025-04-16 17:59:05 -0500

    Update compileSdk and targetSdk to 35; add missing glslang-related libraries to CMake configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.yolo_kotlin_ncnn"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.yolo_kotlin_ncnn"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
